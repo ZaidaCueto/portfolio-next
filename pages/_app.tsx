2ddb090d327bdb61511daf400c777995771d04b4
zaida cueto <zcueto58@gmail.com>
2022-07-24 11:15:18 -0300

    configuration file _document.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="grid grid-cols-12 gap-6 px-5 lg:48 my-14">
+      <div className="col-span-12 p-4 text-center bg-white rounded-2xl lg:col-span-3" >
+       < Sidebar/>
+      </div>
+      <div className="col-span-12 bg-white lg:col-span-9 rounded-2xl">
+        <Navbar/>
+        <Component {...pageProps} />
+      </div>
+    </div>
+  )
+
+
 }
 
 export default MyApp

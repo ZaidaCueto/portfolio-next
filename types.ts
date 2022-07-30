95faee9abef6b005bf1e10bd8b321a8727348fb8
zaida cueto <zcueto58@gmail.com>
2022-07-30 19:48:50 -0300

    configurando intercafe

---
+++ types.ts
@@ -0,0 +1,34 @@
+import { IconType } from 'react-icons'
+
+export interface Service {
+  title: string
+  about: string
+  Icon: IconType
+}
+
+export interface ISkill {
+  name: string
+  Icon: IconType
+}
+
+export interface IProject {
+  name: string
+  description: string
+  image_path: string
+  deployed_url: string
+  github_url: string
+  figma_url?: string
+  category: Category[]
+  key_techs: string[]
+}
+export type Category =
+  | 'react'
+  | 'node'
+  | 'express'
+  | 'typescript'
+  | 'javascript'
+  | 'mysql'
+  | 'all'
+  | 'bootstrap'
+  | 'figma'
+export type Urls = 'deployed_url' | 'github_url' | 'figma_url'

73d4c0e8241a7303663535f4187eecbfce7cf5ac
zaida cueto <zcueto58@gmail.com>
2022-07-30 19:39:38 -0300

    feat: adding darck mode

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,17 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-
-
   render() {
     return (
-      <Html>
-        <Head >
+      <Html lang="pt-BR">
+        <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap"
+            rel="stylesheet"
+          />
         </Head>
-        <body className='bg-gradient-to-r from-purple-300 to-purple-900'>
+        <body className="bg-gradient-to-r from-purple-300 to-purple-900 dark:from-gray-900 dark-to-gray-500 dark:text-purple-500">
           <Main />
           <NextScript />
         </body>

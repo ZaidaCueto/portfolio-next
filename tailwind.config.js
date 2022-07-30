f57eae773997de3c1b48bae206532c8b28156f37
zaida cueto <zcueto58@gmail.com>
2022-07-30 19:47:13 -0300

    configuring tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,56 @@
 module.exports = {
  
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-   darkMode: false, // or 'media' or 'class'
-   theme: {
-     extend: {},
-   },
-   variants: {
-     extend: {},
-   },
-   plugins: [],
- }
+    content: [
+      // Example content paths...
+      "./pages/**/*.tsx", "./components/**/*.tsx"
+    ],
+  
+  darkMode: 'class',
+  theme: {
+    
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'tiny': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+    },
+    fontFamily: {
+      sans: ['ui-sans-serif', 'system-ui'],
+      kaushan: ['Kaushan Script'],
+    },
+    boxShadow: {
+      'custom-light': ' 0 0 10px #313131',
+      'custom-dark': '5px 5px 10px #0a0c0e , -5px -5px 10px #14161c',
+    },
+
+    extend: {
+      // colors: {
+      //   green: {
+      //     DEFAULT: '#00f260',
+      //   },
+      //   dark: {
+      //     DEFAULT: '#010101',
+      //     100: '#0a0b0e',
+      //     200: '#16181d',
+      //     300: '#16181d',
+      //     500: '#0f1115',
+      //     700: '#202125',
+      //   },
+      // },
+    },
+  },
+  variants: {
+    extend: {
+      boxShadow: ['dark'],
+    },
+  },
+  plugins: [],
+}
